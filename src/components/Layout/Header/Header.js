95e2a65840a0f80d0280acf66238295f81c8fd95
Laks96 <lazarspasic96@gmail.com>
2020-10-02 15:53:26 +0200

    Add animation for button and homepage text

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,17 +1,20 @@
 
 import React from 'react'
-import  classes from './Header.module.css'
+import classes from './Header.module.css'
 import Button from '../../UI/Button/Button'
 
 const Header = (props) => {
 
-    return <header className = {classes.header}>
-        <p className = {classes.homeText}>
+    return <header className={classes.header}>
+        <div className = {classes.infoBox}>
+        <p className={classes.homeText}>
             Share something what's <br /> on your mind.
         </p>
-        <Button className = {classes.articleBtn}>Articles</Button>
-        <Button className = {classes.authBtn}>Log in</Button>
-        <Button className = {classes.homePageBtn}>Let's write something</Button>
+        <Button className={classes.homePageBtn}>Let's write something</Button>
+        </div>
+        <Button className={classes.articleBtn}>Articles</Button>
+        <Button className={classes.authBtn}>Log in</Button>
+
     </header>
 
 }
