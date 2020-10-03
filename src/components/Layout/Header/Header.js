f814fca86e4e05734b8523ff6aba5ad93d86e428
Laks96 <lazarspasic96@gmail.com>
2020-10-03 20:18:26 +0200

    And hover effects for get started button

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -2,20 +2,24 @@
 import React from 'react'
 import classes from './Header.module.css'
 import Button from '../../UI/Button/Button'
+import {NavLink} from 'react-router-dom'
 
 const Header = (props) => {
 
-    return <header className={classes.header}>
+    return <> <header className={classes.header}>
         <div className = {classes.infoBox}>
         <p className={classes.homeText}>
             Share something what's <br /> on your mind.
         </p>
-        <Button className={classes.homePageBtn}>Let's write something</Button>
+        <NavLink to = 'auth'>  <Button className={classes.homePageBtn}>Get started</Button></NavLink>
+      
         </div>
         <Button className={classes.articleBtn}>Articles</Button>
         <Button className={classes.authBtn}>Log in</Button>
 
     </header>
+    <div>eee</div>
+    </>
 
 }
 
