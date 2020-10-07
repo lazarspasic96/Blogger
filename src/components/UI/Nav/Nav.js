04bbcfef8e053df1a6c91e3676b1b7a44048244f
Laks96 <lazarspasic96@gmail.com>
2020-10-07 19:11:34 +0200

    Add navigation items

--- src/components/UI/Nav/Nav.js
+++ src/components/UI/Nav/Nav.js
@@ -1,22 +1,43 @@
 import React from 'react'
 import classes from './NavBar.module.scss'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import Button from '../Button/Button'
 
-const NavBar = (props) => {
+class NavBar extends React.Component {
+    state = {
+        toogleClass: false
+    }
 
-    return (
-        <>
-            <nav>
-                <NavLink to='articles'>
-                    <Button className={classes.articleBtn}>Articles</Button>
-                </NavLink>
-                <NavLink to='#auth'>
-                    <Button className={classes.authBtn}>Log in</Button>
-                </NavLink>
-            </nav>
-        </>
-    )
+
+    render() {
+        return (
+            <>
+                <div className={classes.navBar}>
+                    <Button className={classes.navBtn}>Toggle</Button>
+
+                    <div className={classes.btnBox}>
+
+                    </div>
+
+                    <nav className={classes.navigation}>
+                        <ul className={classes.navigationList}>
+                            <li className={classes.item}> <Link className={classes.navLink} to='/'>Log in </Link></li>
+                            <li className={classes.item}> <Link className={classes.navLink} to='/'>Articles </Link></li>
+                            <li className={classes.item}> <Link className={classes.navLink} to='/'>Our Story </Link></li>
+                            <li className={classes.item}> <Link className={classes.navLink} to='/'> Get Stared </Link></li>
+
+                        </ul>
+                    </nav>
+
+
+
+
+                </div>
+
+            </>
+        )
+    }
 }
 
 export default NavBar
+
