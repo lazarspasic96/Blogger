71a8947c732217664b54368c07df431b7641794b
Laks96 <lazarspasic96@gmail.com>
2020-10-19 22:57:55 +0200

    Updates

---
+++ precache-manifest.d0fcd9e23e34aaa413fb358302538a35.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba86f6687528f18d7c5d8344e2a7ce1b",
+    "url": "/Blogger/index.html"
+  },
+  {
+    "revision": "dd563c75746d6ef4237a",
+    "url": "/Blogger/static/css/main.795baefa.chunk.css"
+  },
+  {
+    "revision": "c5f06824bbd50b3549b3",
+    "url": "/Blogger/static/js/2.b46babb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Blogger/static/js/2.b46babb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd563c75746d6ef4237a",
+    "url": "/Blogger/static/js/main.680ce2d7.chunk.js"
+  },
+  {
+    "revision": "d2df15929782b6af85f2",
+    "url": "/Blogger/static/js/runtime-main.98a8b26f.js"
+  },
+  {
+    "revision": "6b7af617e311375a3cc93759a9350a35",
+    "url": "/Blogger/static/media/addPost.6b7af617.svg"
+  },
+  {
+    "revision": "8266fe12ea3bcb19810219914437ab98",
+    "url": "/Blogger/static/media/articleImg.8266fe12.jpg"
+  },
+  {
+    "revision": "f7f12d8b700f6d8f96ca0c84a7a366e3",
+    "url": "/Blogger/static/media/authorization.f7f12d8b.svg"
+  },
+  {
+    "revision": "7a8ed3a307b70e9f162d5d1bb757444c",
+    "url": "/Blogger/static/media/bloggerLogo.7a8ed3a3.svg"
+  },
+  {
+    "revision": "7b252b1c6747476980c8d493c8a7fe7a",
+    "url": "/Blogger/static/media/dashboard.7b252b1c.svg"
+  },
+  {
+    "revision": "8fa6a50b50f3cb82179d04296f094b19",
+    "url": "/Blogger/static/media/discover.8fa6a50b.svg"
+  },
+  {
+    "revision": "42cb05bb471e32206fc61991e02f4a1d",
+    "url": "/Blogger/static/media/edit.42cb05bb.svg"
+  },
+  {
+    "revision": "76e611008a34b0beb1b461d6bd25de3a",
+    "url": "/Blogger/static/media/heart.76e61100.svg"
+  },
+  {
+    "revision": "70f037b5a200e504c14e76020d6188ef",
+    "url": "/Blogger/static/media/homepage.70f037b5.jpg"
+  },
+  {
+    "revision": "c84b307ba4e6817866f1720503bab3be",
+    "url": "/Blogger/static/media/logout.c84b307b.svg"
+  },
+  {
+    "revision": "79da3a53b627c6e776c7276a674b09be",
+    "url": "/Blogger/static/media/myArticles.79da3a53.svg"
+  },
+  {
+    "revision": "d6f07d7783eefbbf4d3654b0b0085dc2",
+    "url": "/Blogger/static/media/user.d6f07d77.svg"
+  }
+]);
